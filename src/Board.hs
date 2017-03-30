c6d1f51c28f29b0c5325000abea940a34edef638
Georgi Gospodinov <georgigospodinov@gmail.com>
2017-03-30 12:13:59 +0100

    evaluation function probably done

--- src/Board.hs
+++ src/Board.hs
@@ -1,7 +1,7 @@
 module Board where
 
 data Col = Black | White
-  deriving Show
+  deriving (Show, Eq)
 
 other :: Col -> Col
 other Black = White
@@ -61,11 +61,37 @@ For every position ((x, y), col) in the 'pieces' list:
 - if n > 1, move one step in direction D, and check for a line of
   n-1 in a row.
 -}
+data Direction = N | E | NE
+descend :: Board -> Col -> Position -> Direction -> Int
+descend b c (x,y) dir = if isC (x,y) then  -- descend up, up-right, right
+                            (1::Int) + descend b c next dir
+                        else 0
+                        where isC :: Position -> Bool
+                              isC pos = case piece_at pos of
+                                            [] -> False
+                                            [piece_found] -> snd piece_found == c
+                              piece_at :: Position -> [(Position, Col)]
+                              piece_at p = filter (\ (pos, c) -> pos == p) (pieces b)
+                              next = case dir of
+                                        N -> (x+1,y)
+                                        E -> (x,y+1)
+                                        NE -> (x+1,y+1)
+
+longest :: Board -> Col -> Int
+longest b c = maximum [max nd (max ed ned) |  -- the largest of the three descensions
+                            nd <- [descend b c (x,y) N | x <- [0..size b -1], y <- [0..size b -1]],  -- descend north
+                            ed <- [descend b c (x,y) E | x <- [0..size b -1], y <- [0..size b -1]],  -- descend east
+                            ned <- [descend b c (x,y) NE | x <- [0..size b -1], y <- [0..size b -1]]  -- descend north-east
+                      ]
 
 -- An evaluation function for a minimax search. Given a board and a colour
 -- return an integer indicating how good the board is for that colour.
 evaluate :: Board -> Col -> Int
-evaluate = undefined
+evaluate b c = if lc == size b -1 then size b -1  -- if c can win from here = best
+               else if loc == size b -1 then 1- size b  -- else if !c can win from here = worst
+               else lc-loc-- otherwise longest c - longest !c
+               where lc = longest b c
+                     loc = longest b $ other c
 
 
 
