831f57161dc0c6df347575a6cde92ff89a680dad
jw255 <jw255@pc3-010-l.cs.st-andrews.ac.uk>
2017-04-12 11:19:09 +0100

    networking setup

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Graphics.Gloss
 import System.Environment
 import Data.String.Utils
+import Network.Socket
 
 import Board
 import Draw
@@ -38,26 +39,46 @@ parseArgument str b =   if startswith "size=" str then
                             b {human = other $ read $ drop (length "computer=") str}
                         else b  -- argument not recognized
 
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+    x <- getArgs
+    conn <- accept sock     -- accept a connection and handle it
+    -- run our server's logic
+    white_piece <- loadBMP "src/img/white.bmp"
+    black_piece <- loadBMP "src/img/black.bmp"
+    cell_pic <- loadBMP "src/img/gomoku-part.bmp"
+    putStrLn "HERE1"
+    play
+      (InWindow "Gomoku"  -- window title
+          (ws x, ws x)
+          (100, 100)  -- window starting position on screen
+      )  --(FullScreen (1,1))  -- currently fails
+      gray  -- background color
+      2  -- 'updateWorld' is called 2 times per second
+      ((wrld x) {blacks=black_piece,whites=white_piece,cell=cell_pic})
+      drawWorld -- in Draw.hs
+      handleInput -- in Input.hs
+      updateWorld -- in AI.hs
+    putStrLn "HERE2"
+      where ws x = win_size $ size $ board $ wrld x
+            wrld x = initWorld {board = foldr parseArgument initBoard x}
+    -- repeat
+    -- mainLoop sock
+
+runConn :: (Socket, SockAddr) -> IO ()
+runConn (sock, _) = do
+    send sock "Hello!\n"
+    close sock
+
 main :: IO ()
-main = do x <- getArgs
-          white_piece <- loadBMP "src/img/white.bmp"
-          black_piece <- loadBMP "src/img/black.bmp"
-          cell_pic <- loadBMP "src/img/gomoku-part.bmp"
-          putStrLn "HERE1"
-          play
-            (InWindow "Gomoku"  -- window title
-                (ws x, ws x)
-                (100, 100)  -- window starting position on screen
-            )  --(FullScreen (1,1))  -- currently fails
-            gray  -- background color
-            2  -- 'updateWorld' is called 2 times per second
-            ((wrld x) {blacks=black_piece,whites=white_piece,cell=cell_pic})
-            drawWorld -- in Draw.hs
-            handleInput -- in Input.hs
-            updateWorld -- in AI.hs
-          putStrLn "HERE2"
-            where ws x = win_size $ size $ board $ wrld x
-                  wrld x = initWorld {board = foldr parseArgument initBoard x}
+main = do
+          sock <- socket AF_INET Stream 0     -- create socket
+          setSocketOption sock ReuseAddr 1    -- make socket immediately reusable - eases debugging.
+          bind sock (SockAddrInet 4242 iNADDR_ANY)   -- listen on TCP port 4242.
+          listen sock 2                              -- set a max of 2 queued connections
+          mainLoop sock                              -- our main loop of accepting connections and doing our logic
+
+
 
 -- play:: Display -> Color -> Int
 -- -> world
